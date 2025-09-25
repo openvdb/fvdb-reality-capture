73299694538cb523e29d1252b6a736b9931a208d
Mark Harris <mharris@nvidia.com>
2025-09-25 23:35:17 +1000

    Add s3 upload/download utils and tests
    Signed-off-by: Mark Harris <mharris@nvidia.com>

--- scripts/upload_to_s3.py
+++ scripts/upload_to_s3.py
@@ -8,28 +8,9 @@ import pathlib
 import sys
 import threading
 
-import boto3
 import tyro
 
-
-class ProgressPercentage(object):
-    """
-    Helper class to report progress of S3 uploads.
-    Taken from: https://boto3.amazonaws.com/v1/documentation/api/latest/guide/s3-uploading-files.html
-    """
-
-    def __init__(self, filename):
-        self._filename = filename
-        self._size = float(os.path.getsize(filename))
-        self._seen_so_far = 0
-        self._lock = threading.Lock()
-
-    def __call__(self, bytes_amount):
-        with self._lock:
-            self._seen_so_far += bytes_amount
-            percentage = (self._seen_so_far / self._size) * 100
-            sys.stdout.write("\r%s %s / %s (%.2f%%)" % (self._filename, self._seen_so_far, self._size, percentage))
-            sys.stdout.flush()
+from fvdb_3dgs.utils import s3
 
 
 def main(source_file_path: pathlib.Path, destination_file_path: pathlib.Path):
@@ -44,25 +25,10 @@ def main(source_file_path: pathlib.Path, destination_file_path: pathlib.Path):
     logging.basicConfig(level=logging.INFO)
     logger = logging.getLogger(__name__)
 
-    if not source_file_path.exists():
-        raise FileNotFoundError(f"File {source_file_path} does not exist.")
-    if not source_file_path.is_file():
-        raise ValueError(f"Path {source_file_path} is not a file.")
-
     fvdb_prefix = "fvdb-reality-capture"
+    bucket = "fvdb-data"
 
-    logger.info(f"Uploading file {source_file_path} to S3 bucket fvdb-data as {fvdb_prefix}/{destination_file_path}...")
-    s3 = boto3.client("s3")
-
-    local_file_path = str(source_file_path)
-    bucket_name = "fvdb-data"
-    s3_object_key = str(pathlib.Path(fvdb_prefix) / str(destination_file_path))
-
-    try:
-        s3.upload_file(local_file_path, bucket_name, s3_object_key, Callback=ProgressPercentage(local_file_path))
-        logger.info(f"File '{local_file_path}' uploaded to S3 bucket '{bucket_name}' as '{s3_object_key}'")
-    except Exception as e:
-        logger.error(f"Error uploading file: {e}")
+    uri = s3.upload(source_file_path, bucket, fvdb_prefix / destination_file_path)
 
 
 if __name__ == "__main__":
