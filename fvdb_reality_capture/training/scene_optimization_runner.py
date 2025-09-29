c18c6c88164d6019261edffc101b631367a55807
Matthew Cong <mcong@nvidia.com>
2025-09-29 08:04:47 -0700

    Disable LPIPS, PSNR, and parallel loading
    Signed-off-by: Matthew Cong <mcong@nvidia.com>

--- fvdb_reality_capture/training/scene_optimization_runner.py
+++ fvdb_reality_capture/training/scene_optimization_runner.py
@@ -54,9 +54,9 @@ class Config:
     # Optional maximum number of training steps (overrides max_epochs * dataset_size if set)
     max_steps: int | None = None
     # Percentage of total epochs at which we perform evaluation on the validation set. i.e. 10 means perform evaluation after 10% of the epochs.
-    eval_at_percent: List[int] = field(default_factory=lambda: [10, 20, 30, 40, 50, 75, 100])
+    eval_at_percent: List[int] = field(default_factory=lambda: [1, 10, 20, 30, 40, 50, 75, 100])
     # Percentage of total epochs at which we save the model checkpoint. i.e. 10 means save a checkpoint after 10% of the epochs.
-    save_at_percent: List[int] = field(default_factory=lambda: [10, 20, 30, 40, 50, 75, 100])
+    save_at_percent: List[int] = field(default_factory=lambda: [1, 10, 20, 30, 40, 50, 75, 100])
 
     #
     # Gaussian Optimization Parameters
