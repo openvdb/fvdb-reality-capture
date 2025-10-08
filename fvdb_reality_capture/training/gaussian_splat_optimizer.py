f48736e1b44c834784ee7ab7b72c03fd1ba8c9a8
Francis Williams <fwilliams@users.noreply.github.com>
2025-10-08 11:07:55 -0400

    Update fvdb_reality_capture/training/gaussian_splat_optimizer.py
    Co-authored-by: Mark Harris <mharris@nvidia.com>
    Signed-off-by: Francis Williams <fwilliams@users.noreply.github.com>

--- fvdb_reality_capture/training/gaussian_splat_optimizer.py
+++ fvdb_reality_capture/training/gaussian_splat_optimizer.py
@@ -347,7 +347,7 @@ class GaussianSplatOptimizer:
         **Duplication**: Make `self._config.insertion_duplication_factor` copies of a Gaussian.
           - We duplicate a Gaussian if its 3D size is below some threshold and the gradient of its projected means over time is high on
             average. Intuitively, this means the Gaussian is not taking up a lot of space in the scene, but consistently wants to change positions
-            when viewed from different images. Likely this Gaussian is stuck trying to represent too much of the scene and should
+            when viewed from different cameras. Likely this Gaussian is stuck trying to represent too much of the scene and should
             be split into multiple copies.
 
         **Splitting**: Split a Gaussian into `self._config.insertion_split_factor` smaller ones.
