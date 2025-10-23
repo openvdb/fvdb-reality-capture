144633a4eeb9da079fce1c54196688b98313a7a5
Jonathan Swartz <jonathan@jswartz.info>
2025-10-24 07:28:00 +1300

    filter function name changes
    Signed-off-by: Jonathan Swartz <jonathan@jswartz.info>

--- fvdb_reality_capture/tools/_filter_splats.py
+++ fvdb_reality_capture/tools/_filter_splats.py
@@ -5,7 +5,7 @@ import torch
 from fvdb import GaussianSplat3d
 
 
-def filter_splat_means(
+def filter_splats_by_mean_percentile(
     splats: GaussianSplat3d, percentile=[0.98, 0.98, 0.98, 0.98, 0.98, 0.98], decimate=4
 ) -> GaussianSplat3d:
     """
@@ -38,7 +38,7 @@ def filter_splat_means(
     return splats[good_inds]
 
 
-def filter_splat_opacities(splats: GaussianSplat3d, percentile=0.98, decimate=4) -> GaussianSplat3d:
+def filter_splats_by_opacity_percentile(splats: GaussianSplat3d, percentile=0.98, decimate=4) -> GaussianSplat3d:
     """
     Remove all gaussians falling outside provided percentile range for logit_opacities.
     Args:
@@ -55,7 +55,7 @@ def filter_splat_opacities(splats: GaussianSplat3d, percentile=0.98, decimate=4)
     return splats[good_inds]
 
 
-def filter_splat_large_scales(splats: GaussianSplat3d, prune_scale3d_threshold=0.05) -> GaussianSplat3d:
+def filter_splats_above_scale(splats: GaussianSplat3d, prune_scale3d_threshold=0.05) -> GaussianSplat3d:
     """
     Remove all gaussians with sizes larger than provided percent threshold (relative to scene scale)
     Args:
@@ -71,5 +71,25 @@ def filter_splat_large_scales(splats: GaussianSplat3d, prune_scale3d_threshold=0
     dists = torch.linalg.norm(points - scene_center, dim=1)
     scene_scale = torch.max(dists) * 1.1
     good_inds = torch.exp(splats.log_scales).max(dim=-1).values < prune_scale3d_threshold * scene_scale
+
+    return splats[good_inds]
+
+
+def filter_splats_below_scale(splats: GaussianSplat3d, prune_scale3d_threshold=0.05) -> GaussianSplat3d:
+    """
+    Remove all gaussians with sizes smaller than provided percent threshold (relative to scene scale)
+    Args:
+        splats: GaussianSplat3d to filter
+        prune_scale3d_threshold: drop all spats with scales smaller than this threshold (relative to scene scale)
+
+    Returns:
+        GaussianSplat3d after removal of gaussians outside threshold
+    """
+
+    points = splats.means
+    scene_center = torch.mean(points, dim=0)
+    dists = torch.linalg.norm(points - scene_center, dim=1)
+    scene_scale = torch.max(dists) * 1.1
+    good_inds = torch.exp(splats.log_scales).max(dim=-1).values > prune_scale3d_threshold * scene_scale
 
     return splats[good_inds]
