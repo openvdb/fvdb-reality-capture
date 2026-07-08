7b354afab1cf15d6e47ee5f4e962658553ba227e
Matthew Cong <mcong@nvidia.com>
2026-07-08 11:58:47 -0700

    Set test tag
    Signed-off-by: Matthew Cong <mcong@nvidia.com>

--- tests/unit/__init__.py
+++ tests/unit/__init__.py
@@ -1,3 +1,7 @@
 # Copyright Contributors to the OpenVDB Project
 # SPDX-License-Identifier: Apache-2.0
 #
+
+from fvdb.utils.tests import set_testing_git_tag
+
+set_testing_git_tag("59e48d3daa8b8fb55a30fd3d7553fc7fa773ab07")
